77b1a166c03454cba269bbc0081f0d8c78864303
dianatecher <unsupermail@gmail.com>
2016-09-06 11:46:52 +0200

    feature [already filled] added + console.log stuffs

--- morpion.js
+++ morpion.js
@@ -1,26 +1,47 @@
+//small squares
 var squares = document.querySelectorAll(".small-square");
-var counter = 0;
+
+//string variables
+var cross = "small-square purple cross";
+var nought = "small-square blue nought";
+
+// o
+var crosses =  document.getElementsByClassName(cross);
+// x
+var noughts =  document.getElementsByClassName(nought);
+var counter = 1;
 
 
-function fill(anElement){
-  if (counter%2 == 0) {
-    anElement.className = "small-square purple cross";
-    counter+=1;
+//let's check the browser console
+function displayMsg() {
+  console.log("counter = "+ counter);
+  console.log("crosses : "+ crosses.length + " | noughts : "+ noughts.length);
+  console.log("squares : "+ squares.length);
+}
+
+
+function fill(anElement) {
+  if ((anElement.className != cross) && (anElement.className != nought)) {
+      if(counter%2 == 0) {
+        anElement.className = cross;
+      } else {
+        anElement.className = nought;
+      }
+        displayMsg();
+        console.log("className : "+ anElement.className);
   } else {
-    anElement.className = "small-square blue nought";
-    counter+=1;
+    alert("already filled !");
+    console.log("already filled !");
   }
 }
 
 
 function play(){
   for (var i = 0; i < squares.length; i++) {
-    if (squares[i].className == "small-square") {
-      squares[i].addEventListener("click", function() { fill(this);});
-      console.log("counter "+ counter);
-    } else {
-      alert("Already filled !");
-    }
+      squares[i].addEventListener("click", function() {
+        fill(this);
+        counter+=1;
+      });
   }//for
 }
 
